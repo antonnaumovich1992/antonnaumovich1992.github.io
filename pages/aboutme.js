b8fbff4fc4b9bddb854f42f4269df7a7246fd4a0
Антон Наумович <antonnaumovich1992@gmail.com>
2019-11-11 22:18:38 +0300

    first commit

---
+++ pages/aboutme.js
@@ -0,0 +1,30 @@
+let aboutme = `
+    
+        <div class="home">
+        <p class="aboutme-content">
+            So if you clicked "About me", then you are interested to know who I am:)
+            <br>
+            My name is Anton, and I'm 27 years old. I live in Minsk now. I've been to Canada few times,I have many friends there so my 
+            English skills are pretty high and I really like to communicate with native speakers.
+            <br>
+            I became pretty good engineer after university but now I realized that future is all about computers and 
+            I really want to become part of that.
+
+            I'm only beginer, but I have great motivation and desire to become a good specialist!
+            
+            I am really patient, sociable and fast learning person. Prefer to work in team and become a leader.
+            <br>
+            My hobbies are: Extreme music, computer hardware and games, motorcycle riding.
+
+            <br>
+
+        
+
+
+
+        </p>
+
+
+        </div>
+
+`;
---
+++ pages/homepage.js
@@ -0,0 +1,19 @@
+let homepage = `
+    <section class="home">
+        <div class="home-content">
+            <h1>Anton Naumovich</h1>
+            <h3>Junior front-end developer | Former Mechanical Engineer</h3>
+        <br>
+        <p>
+            My name is Anton. Im 27 y.o. and I am a former Mechanical Enjineer.
+            This is simple example of single page app with JavaScript
+        </p>
+
+       
+        </div>
+
+        <img class="portrait-image" src="https://sun9-53.userapi.com/c851320/v851320521/c5130/DHM7Kc0_F-U.jpg">
+
+        
+    </section>
+`;
